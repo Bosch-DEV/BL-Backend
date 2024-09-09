ad760bf9dfc2b9c60be033a8a71d23cd9f40d496
Bedlessdeus <bedlesssgod@gmail.com>
2024-09-09 21:53:58 +0200

    Added Runner

---
+++ Runner.cs
@@ -0,0 +1,26 @@
+﻿namespace LauncherBackEnd;
+
+public class Runner {
+
+    private static bool running = true;
+    private static Thread? keepAliveThread = null;
+
+    private static void Yes() {
+        while (running) {
+            _ = 1;
+        }
+    }
+
+    public static void KeepAliveMultiThreaded() {
+        keepAliveThread = new Thread(() => {
+            Thread.CurrentThread.IsBackground = false;
+            Yes();
+        });
+        keepAliveThread.Start();
+    }
+
+    public static void StopRunner() {
+        running = false;
+        keepAliveThread?.Join();
+    }
+}
